8e5d2f0abb0c550b2e7896f4593a14ae1f7ff8e2
Taylor Watson <taylorwatsonb@gmail.com>
2019-07-10 19:23:45 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,27 +1,32 @@
+
+ 
 function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
+var lyric = "maybe";
   return lyric;
-  var lyric = "maybe";
+ 
 }
 
+console.log("I just met you...");
+console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
 
+
+thisIsCrazy();
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
 }
-
+sayMyName();
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
+  
 
   function sayMy() {
     console.log(name);
